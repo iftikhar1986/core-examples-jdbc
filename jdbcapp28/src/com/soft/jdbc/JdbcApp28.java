7a226cef6891042efc6ac43be8adc8faad6e10cd
iftikhar555 <iftikhar.qt@gmail.com>
2020-02-17 21:41:18 +0300

    JDBC Program by using Msql Database

---
+++ jdbcapp28/src/com/soft/jdbc/JdbcApp28.java
@@ -0,0 +1,42 @@
+package com.soft.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JdbcApp28 {
+
+	public static void main(String[] args) {
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/durgadb","root","root");
+			st = con.createStatement();
+			rs = st.executeQuery("select * from emp1");
+			System.out.println("ENO\tENAME\tESAL\tEADDR");
+			System.out.println("-------------------------");
+			while (rs.next()) {
+				System.out.print(rs.getInt("ENO")+"\t");
+				System.out.print(rs.getString("ENAME")+"\t");
+				System.out.print(rs.getFloat("ESAL")+"\t");
+				System.out.print(rs.getString("EADDR")+"\n");
+				
+			}
+			//System.out.println();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				con.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+			
+			
+		}
+	}
+
+}
